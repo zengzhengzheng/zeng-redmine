61698998de81526794c86d3b0539220f45053dc2
cndaniel <tigerljt@gmail.com>
2016-12-04 16:27:05 +0800

    mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,21 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
+
+ #mailgun
+ config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  port:           587,
+  address:        "smtp.mailgun.org",
+  user_name:      ENV["mailgun_user"],
+  password:       ENV["mailgun_secret"],
+  domain:         "dasredmine.herokuapp.com", # 你的 mailgun domain name
+
+  authentication: :plain,
+
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
